daf09ac39032fc9e69b572de01784d116eaca7f7
TokhirAsadov <guvalakat1603@gmail.com>
2022-04-22 18:42:10 +0500

    Create NavLinks.jsx component for Navbar

--- src/components/NavLinks.jsx
+++ src/components/NavLinks.jsx
@@ -1,11 +1,82 @@
 import React from 'react';
+import styled from "styled-components";
+import SearchIcon from '@mui/icons-material/Search';
 
-const Footer = () => {
+const NavLinks = () => {
+
+    const Container = styled.div`
+      flex : 2;
+      display: flex;
+      align-items: center;
+      justify-content: space-around;
+    `;
+
+    const LinkWrapper = styled.span`
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      cursor: pointer;
+    `;
+
+    const NavLinks = styled.a`
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      min-width: 100%;
+      padding: 5px 8px;
+      font-size: 12px;
+      font-weight: bold;
+      color: #fff;
+      text-decoration: none;
+      letter-spacing: 1.1px;
+
+      &:hover {
+        background-color: #ac77ac;
+      }
+    `;
+
     return (
-        <div>
-            Footer
-        </div>
+        <Container>
+            <LinkWrapper>
+                <NavLinks>
+                    Home
+                </NavLinks>
+            </LinkWrapper>
+            <LinkWrapper>
+                <NavLinks>
+                    About
+                </NavLinks>
+            </LinkWrapper>
+            <LinkWrapper>
+                <NavLinks>
+                    Computer
+                </NavLinks>
+            </LinkWrapper>
+            <LinkWrapper>
+                <NavLinks>
+                    Laptop
+                </NavLinks>
+            </LinkWrapper>
+            <LinkWrapper>
+                <NavLinks>
+                    Products
+                </NavLinks>
+            </LinkWrapper>
+            <LinkWrapper>
+                <NavLinks>
+                    Contact Us
+                </NavLinks>
+            </LinkWrapper>
+            <LinkWrapper>
+                <SearchIcon style={{fontSize: "18px",padding:"4px",borderRadius:"50%",color:"#fff"}}/>
+            </LinkWrapper>
+            <LinkWrapper>
+                <NavLinks>
+                    Login
+                </NavLinks>
+            </LinkWrapper>
+        </Container>
     );
 };
 
-export default Footer;
+export default NavLinks;
