e49b90e798b6510adaa34b06f303659b2fd2b82d
TokhirAsadov <guvalakat1603@gmail.com>
2022-04-22 18:41:46 +0500

    Create Logo.jsx component

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,11 +1,38 @@
 import React from 'react';
+import styled from "styled-components";
 
-const Footer = () => {
+const Logo = () => {
+
+    const Container = styled.div`
+      flex: 1;
+    `;
+
+    const Wrapper = styled.div`
+      width: 40px;
+      height: 40px;
+      border: 4px solid lime;
+      padding: 3px;
+      border-radius: 50%;
+      margin-left: 100px;
+      cursor: pointer;
+    `;
+
+    const Logo = styled.a`
+      min-width: 100%;
+      letter-spacing: 1.1px;
+      font-weight: bold;
+      color: #fff;
+    `
+
     return (
-        <div>
-            Footer
-        </div>
+        <Container>
+            <Wrapper>
+                <Logo>
+                    CLA
+                </Logo>
+            </Wrapper>
+        </Container>
     );
 };
 
-export default Footer;
+export default Logo;
