ab2970a2ec56c460bb32e9455cfd5975d6b3eb97
TokhirAsadov <guvalakat1603@gmail.com>
2022-04-23 20:31:41 +0500

    horizontal slide created successfully

--- src/components/RightFromHomeViewSection.jsx
+++ src/components/RightFromHomeViewSection.jsx
@@ -1,13 +1,65 @@
 import React from 'react';
 import styled from "styled-components";
+import img1 from "./../utill/images/1.png"
+import img2 from "./../utill/images/2.png"
+import img3 from "./../utill/images/3.png"
+import img4 from "./../utill/images/4.png"
+import img5 from "./../utill/images/5.png"
+import {Splide, SplideSlide} from "@splidejs/react-splide";
+import '@splidejs/splide/dist/css/splide.min.css';
 
 const RightFromHomeViewSection = () => {
+
+    const images = [img1,img2,img3,img4,img5];
+
     const Container = styled.div`
       flex: 1;
-    `
+      padding: 20px 30px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    `;
+
+    const Wrapper = styled.span`
+      width: 500px;
+      height: 500px;
+      overflow: hidden;
+      margin-top: 50px;
+    `;
+
+    const Card = styled.div` 
+      min-height: 25rem;
+      border-radius: 2rem;
+      overflow: hidden;
+      
+      img{
+        width: 500px;
+        height: 400px;
+      }
+    `;
+
     return (
         <Container>
-            RightFromHomeViewSection
+            <Wrapper>
+                <Splide options={{
+                    perPage: 1,
+                    // arrows: false,
+                    drag: 'free',
+
+                }}>
+                    {
+                        images.map((recipe) => {
+                            return (
+                                <SplideSlide style={{}}>
+                                    <Card>
+                                        <img src={recipe}/>
+                                    </Card>
+                                </SplideSlide>
+                            )
+                        })
+                    }
+                </Splide>
+            </Wrapper>
         </Container>
     );
 };
