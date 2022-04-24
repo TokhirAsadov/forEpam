cc0429eb54b1de8ee6817032a1e0e00d6a0457fa
TokhirAsadov <guvalakat1603@gmail.com>
2022-04-24 19:20:15 +0500

    fixed some code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import HomeViewSection from "../components/HomeViewSection";
 import ThreeCards from "./ThreeCards";
+import Products from "./Products";
+import Contact from "./Contact";
+import Computer from "./Computer";
+import styled from "styled-components";
 
 const Home = () => {
+    const Wrapper = styled.div`
+      margin-top: 69px;
+    `;
     return (
-        <div>
+        <Wrapper>
             <HomeViewSection />
             <ThreeCards />
-        </div>
+            <Products marginTop={"0px"}/>
+            <Computer marginTop={"0px"}/>
+            <Contact marginTop={"0px"}/>
+        </Wrapper>
     );
 };
 
